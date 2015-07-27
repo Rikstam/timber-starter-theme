f35a3a15ea6a45c88cd50d8683347a47db998d80
Riku Kestilä <rikukestila@gmail.com>
2015-07-27 14:09:07 +0300

    add script and style loading to functions

--- functions.php
+++ functions.php
@@ -22,7 +22,25 @@ class StarterSite extends TimberSite {
 		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
 		add_action( 'init', array( $this, 'register_post_types' ) );
 		add_action( 'init', array( $this, 'register_taxonomies' ) );
+
+		add_action( 'init', array( $this, 'load_footer_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'load_styles' ) );
+
 		parent::__construct();
+	}
+
+	function load_footer_scripts()
+	{
+			if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
+					wp_register_script('mainscripts', get_template_directory_uri() . '/static/js/min/site-min.js', array('jquery'), '1.0.0', true); // Custom scripts
+					wp_enqueue_script('mainscripts'); // Enqueue it!
+			}
+	}
+	function load_styles()
+	{
+
+			wp_register_style('timberstyles', get_template_directory_uri() . '/static/css/main.css', array(), '1.0', 'all');
+			wp_enqueue_style('timberstyles'); // Enqueue it!
 	}
 
 	function register_post_types() {
